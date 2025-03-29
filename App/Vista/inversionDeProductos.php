b373147972f209de71547e8b4e5c090b8573eaf7
Englis666 <eabarros2610@gmail.com>
2025-03-29 18:32:15 -0500

    Feature: <InversionDeProductos.php> : Realizada la vista (Arreglar Footer)

--- App/Vista/inversionDeProductos.php
+++ App/Vista/inversionDeProductos.php
@@ -196,7 +196,7 @@
 
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Gestion de productos</h1>
+                        <h1 class="h3 mb-0 text-gray-800">Inversion De Productos</h1>
                         <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> Generar Reporte
                         </a>
@@ -256,6 +256,7 @@
                                             <th>Nombre del producto</th>
                                             <th>Precio de compra al proovedor</th>
                                             <th>Nombre del proovedor</th>
+                                            <th>Fecha y hora</th>
                                         </thead>
                                         <tbody>
                                             <tr>
@@ -297,89 +298,6 @@
                             </div>
                         </div>
                     </div>
-                    
-                    <!-- Content Row -->
-                    <div class="row">
-                         <!-- Stock de Productos Por Categoria -->
-                        <div class="col-lg-6 mb-4">
-                            <div class="card shadow mb-4">
-                                <div class="card-header py-3">
-                                    <h6 class="m-0 font-weight-bold text-primary">Stock de Productos Por Categoria</h6>
-                                </div>
-                                <div class="card-body">
-                                    <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-danger" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-warning" role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar" role="progressbar" style="width: 60%" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-                                    <div class="progress mb-4">
-                                        <div class="progress-bar bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                    <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-                                    <div class="progress">
-                                        <div class="progress-bar bg-success" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <!-- Leyenda para la gráfica -->
-                        <div class="col-lg-6 mb-4">
-                            <p>Leyenda para la gráfica</p>
-                            <div class="row">
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-primary text-white shadow">
-                                        <div class="card-body">Primary<div class="text-white-50 small">#4e73df</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-success text-white shadow">
-                                        <div class="card-body">Success<div class="text-white-50 small">#1cc88a</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-info text-white shadow">
-                                        <div class="card-body">Info<div class="text-white-50 small">#36b9cc</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-warning text-white shadow">
-                                        <div class="card-body">Warning<div class="text-white-50 small">#f6c23e</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-danger text-white shadow">
-                                        <div class="card-body">Danger<div class="text-white-50 small">#e74a3b</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-secondary text-white shadow">
-                                        <div class="card-body">Secondary<div class="text-white-50 small">#858796</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-light text-black shadow">
-                                        <div class="card-body">Light<div class="text-black-50 small">#f8f9fc</div></div>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="card bg-dark text-white shadow">
-                                        <div class="card-body">Dark<div class="text-white-50 small">#5a5c69</div></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                <!-- /.container-fluid -->
-
             </div>
             <!-- End of Main Content -->
 
