e48edba244db16efb895d0f7bb5bc6e740b59d6b
Englis666 <eabarros2610@gmail.com>
2025-03-29 18:19:57 -0500

    Fix : <AgregarProductoModal.php> : se arrregla unos detalles de la estructura

--- App/Vista/Modals/AgregarProductoModal.php
+++ App/Vista/Modals/AgregarProductoModal.php
@@ -9,60 +9,49 @@
 </head>
 <body>
 
-<button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalIframe">Abrir Modal</button>
 
 <div class="modal fade" id="modalIframe" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modalLabel">agregarProductos</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="modalLabel">Agregar Producto</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Agregar Un Producto</h1>
                 </div>
-                <div class="modal-body">
-                    <iframe src="modal.php" width="100%" height="300px" style="border:none;"></iframe>
-                    <div class="p-5">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Agregar Un Producto</h1>
-                            </div>
-                            <form class="user">
-                                <div class="form-group row">
-                                    <div class="col-sm-6 mb-3 mb-sm-0">
-                                        <input type="text" class="form-control form-control-user"
-                                            placeholder="Codigo de barras">
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <input type="text" class="form-control form-control-user" 
-                                            placeholder="Nombre del producto">
-                                    </div>
-                                     <div class="col-sm-6 mt-2">
-                                        <input type="number" class="form-control form-control-user"
-                                            placeholder="Cantidad de producto">
-                                    </div>
-                                    <div class="col-sm-6 mt-2">
-                                        <input type="number" class="form-control form-control-user" 
-                                            placeholder="Precio del producto">
-                                    </div>
-
-                                    <div class="col-sm-6 mt-2">
-                                        <select name="" id="">
-                                            <option value="">Selecciona la categoria del producto</option>
-                                            <option value=""></option>
-                                        </select>
-                                    </div>
-                                    
-                                </div>
-                                <div class="d-flex justify-content-center mt-3">
-                                <a href="login.html" class="btn btn-primary btn-user btn-block">
-                                    Agregar Producto
-                                </a>
-                                <hr>
-                                </div>
-                            </form>
+                <form class="user">
+                    <div class="form-group row">
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                            <input type="text" class="form-control form-control-user" placeholder="Código de barras">
                         </div>
-                </div>
+                        <div class="col-sm-6">
+                            <input type="text" class="form-control form-control-user" placeholder="Nombre del producto">
+                        </div>
+                        <div class="col-sm-6 mt-2">
+                            <input type="number" class="form-control form-control-user" placeholder="Cantidad de producto">
+                        </div>
+                        <div class="col-sm-6 mt-2">
+                            <input type="number" class="form-control form-control-user" placeholder="Precio del producto">
+                        </div>
+                        <div class="col-sm-6 mt-2">
+                            <select class="form-control">
+                                <option value="">Selecciona la categoría del producto</option>
+                                <option value="1">Electrónica</option>
+                                <option value="2">Ropa</option>
+                                <option value="3">Alimentos</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center mt-3">
+                        <button type="submit" class="btn btn-primary btn-user btn-block">Agregar Producto</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
 </body>
 </html>
