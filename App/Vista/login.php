82493fe3c571998f88b57ca9a302c87d13e2601e
Englis666 <eabarros2610@gmail.com>
2025-03-29 17:53:21 -0500

    Cambios

--- App/Vista/login.php
+++ App/Vista/login.php
@@ -1,9 +1,3 @@
-<?php session_start(); ?>
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -15,39 +9,45 @@ ini_set('display_errors', 1);
 </head>
 <body>
 
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-mb-4">
-                <h1 class="text center">Bievenido al sistema Artus Facturacion</h1>
-                <h2 class="text-center">Iniciar sesion</h2>
-                <?php if (isset($_SESSION['error'])): ?>
-                    <div class="alert alert-danger"><?= $_SESSION['error']; unset($_SESSION['error']); ?></div>
-                <?php endif; ?>
-                <form method="POST" action="login.php" class="form">
-                    <div class="mb-3">
-                        <label class="form-label">Numero de documento</label>
-                        <input type="number" name="num_doc" class="form-control" required>
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label">Nombre Completo</label>
-                        <input type="text" name="usuario" class="form-control" required>
-                     </div>
+    <section class="vh-100" style="background-color:rgb(39, 37, 37);">
+  <div class="container py-5 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-10">
+        <div class="card" style="border-radius: 1rem;">
+              <div class="card-body p-4 p-lg-5 text-black">
 
-                    <div class="mb-3">
-                         <label class="form-label">Contraseña</label>
-                         <input type="text" name="password" class="form-control" required>
-                    </div>
+                <form>
 
-                    <button type="submit" class="btn btn-primary w-100">Iniciar Sesion</button>
-                    <a href="registro.php" class="d-block text-center mt-2 btn btn-secondary">Registrarse</a>
+                  <div class="d-flex align-items-center mb-3 pb-1">
+                    <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
+                    <span class="h1 fw-bold mb-0 text-center">Logo</span>
+                  </div>
 
+                  <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Inicia sesion</h5>
+
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="number" name="num_doc" class="form-control form-control-lg" />
+                    <label class="form-label" for="form2Example17">Numero de documento</label>
+                  </div>
+
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <input type="password" name="password" class="form-control form-control-lg" />
+                    <label class="form-label">Contraseña</label>
+                  </div>
+
+                  <div class="pt-1 mb-4">
+                    <button data-mdb-button-init data-mdb-ripple-init class="btn btn-dark btn-lg btn-block" type="button">Login</button>
+                  </div>
+                  <a href="#!" class="small text-muted">Terminos de uso</a>
+                  <a href="#!" class="small text-muted">Politica de privacidad</a>
                 </form>
 
-            </div>
+              </div>
         </div>
+      </div>
     </div>
-    
-
+  </div>
+</section>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
