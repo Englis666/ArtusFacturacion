e89895e57813bed075271fc577589e92c1f811e5
Englis666 <eabarros2610@gmail.com>
2025-04-03 20:16:13 -0500

    asd

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header('Location: /ArtusFacturacion/Public/');
+exit;
--- router.php
+++
@@ -1,8 +0,0 @@
-<?php
-$request_uri = $_SERVER['REQUEST_URI'];
-
-$file_path = __DIR__ . parse_url($request_uri, PHP_URL_PATH);
-if (file_exists($file_path) && is_file($file_path)) {
-    return false; 
-}
-require 'Public/index.php';
