8541d7869b9ed26bdf35096d412f1360c4b20849
Englis666 <eabarros2610@gmail.com>
2025-06-06 22:55:37 -0500

    Add: Create backend_routes.php for defining backend routing logic

---
+++ App/Config/backend_routes.php
@@ -0,0 +1,14 @@
+<?php
+return [
+   'registrar' => ['auth', 'registrar'],
+    'logearse' => ['auth', 'logearse'],
+    'logout' => ['auth', 'logout'],
+    'agregarProducto' => ['producto', 'agregarProducto'],
+    'agregarCategoria' => ['categoria', 'agregarCategoria'],
+    'agregarProveedor' => ['proveedor', 'agregarProveedor'],
+    'generarReporteProductos' => ['reporte', 'generarReporteProductos'],
+    'generarReporteProveedores'=> ['reporte', 'generarReporteProveedores'],
+    'generarReporteInversiones' => ['reporte', 'generarReporteInversiones'],
+    'generarReporteGanancias' => ['reporte', 'generarReporteGanancias'],
+    'agregarVenta' => ['venta', 'agregarVenta'],
+];
