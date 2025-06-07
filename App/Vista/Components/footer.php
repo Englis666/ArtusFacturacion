b02df55cea0dc23da03a23adc4b1ed10fc97ae2c
Englis666 <eabarros2610@gmail.com>
2025-06-06 22:47:25 -0500

    Add: Implement footer component with copyright information

---
+++ App/Vista/Components/footer.php
@@ -0,0 +1,9 @@
+
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Artus Sistema De Inventario Y Facturacion</span>
+                    </div>
+                </div>
+            </footer>
+    
