661512dc019a4280543919b2503ec6e5e933d6f6
Englis666 <eabarros2610@gmail.com>
2025-06-06 22:55:39 -0500

    Add: Create frontend_routes.php for defining frontend routing logic

---
+++ App/Config/frontend_routes.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'login' => ['vista', 'mostrar'],
+    'registro' => ['vista', 'mostrar'],
+    'dashboard' => ['vista', 'mostrar'],
+    'ProductosStock' => ['vista', 'mostrar'],
+    'Proveedores' => ['vista', 'mostrar'],
+    'gananciaDeProductos' => ['vista', 'mostrar'],
+    'inversionDeProductos' => ['vista', 'mostrar'],
+    'formularioVenta' => ['vista' , 'mostrar'],
+];
