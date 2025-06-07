93aea79a661d2a0dfb4878985e5fd289151454a1
Englis666 <eabarros2610@gmail.com>
2025-06-06 22:47:02 -0500

    Refactor: Simplify dashboard layout by including sidebar, navbar, and footer components

--- App/Vista/dashboard.php
+++ App/Vista/dashboard.php
@@ -53,156 +53,18 @@ function obtenerColor($porcentaje) {
     <!-- Page Wrapper -->
     <div id="wrapper">
 
+        <?php include 'App/Vista/Components/sidebar.php'; ?>
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="dashboard">
-                <div class="sidebar-brand-icon">
-                     <i class="fas fa-globe"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Mundo Accesorio</div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="dashboard">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Inicio</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interfaces
-            </div>
-
-            <!-- Inventario -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#Inventario"
-                    aria-expanded="false" aria-controls="Inventario">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Inventario</span>
-                </a>
-                <div id="Inventario" class="collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionSidebar">
-                    <div class=" py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Productos y Proveedores:</h6>
-                        <a class="collapse-item text-white" href="Proveedores">Proveedores</a>
-                        <a class="collapse-item text-white" href="ProductosStock">Productos Stock</a>
-                    </div>
-                </div>
-            </li>
-                            
-
-            <!-- Tienda -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#Tienda"
-                    aria-expanded="false" aria-controls="Tienda">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Tienda</span>
-                </a>
-                <div id="Tienda" class="collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionSidebar">
-                    <div class=" py-2 collapse-inner rounded">
-                        <a class="collapse-item text-white" href="inversionDeProductos">Inversiones de productos</a>
-                        <a class="collapse-item text-white" href="gananciaDeProductos">Ganancias de productos</a>
-                    </div>
-                </div>
-            </li>
-            
-             <li class="nav-item active" >
-                <a class="nav-link" href="/logout">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Cerrar Sesion</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Adicionales
-            </div>
-
-            <!-- Estos son adicionales -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                    aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-folder"></i>
-                    <span>Vistas</span>
-                </a>
-                <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">SubVistas</h6>
-                        <a class="collapse-item" href="">Ver proveedores</a>
-                    </div>
-                </div>
-            </li>
-
-            
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
-        <!-- Fin del sidebar -->
+        
 
         <!-- Contenido -->
         <div id="content-wrapper" class="d-flex flex-column">
+            
+            <?php include 'App/Vista/Components/navbar.php'; ?>
 
             <!-- Main Content -->
             <div id="content">
 
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Nombre completo del administrador</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Registro de Actividad
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Cerrar Sesion
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
@@ -355,16 +217,9 @@ function obtenerColor($porcentaje) {
 
             </div>
             <!-- End of Main Content -->
-
+            <?php include 'App/Vista/Components/footer.php'; ?>
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Artus Sistema De Inventario Y Facturacion</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
+            
 
         </div>
         <!-- End of Content Wrapper -->
